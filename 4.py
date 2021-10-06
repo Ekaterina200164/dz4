d2cb3646e496e513cab5c30708cfd6c131c82c76
Ekaterina200164 <ekaterina200164@gmail.com>
2021-10-06 03:45:33 +0300

    4_1

--- 4.py
+++ 4.py
@@ -1,11 +1,5 @@
 l = list(map(int, input().split()))
-def even(x):
-    k = 0
-    for i in l:
-        if i % 2 == 0:
-            k += 1
-    return k 
-def decorator(foo):
+def decorator(even):
     def wrapper(x):
         if even(x) == 0:
             return 'нет'
@@ -14,4 +8,11 @@ def decorator(foo):
         else:
             return even(x)
     return wrapper
-print(decorator(even)(l))
+@decorator
+def even(x):
+    k = 0
+    for i in x:
+        if i % 2 == 0:
+            k += 1
+    return  k
+print(even(l))
